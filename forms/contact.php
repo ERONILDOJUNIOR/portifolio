0991f6aa7d8d1b1152f90fa972d0114f4627e4b7
Eronildo Júnior <eronjr17.ej@gmail.com>
2022-12-14 18:01:55 -0300

    test email 3

--- forms/contact.php
+++ forms/contact.php
@@ -24,19 +24,18 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'esmtp-mail.outlook.com',
+    'username' => 'eronjr17.ej@outlook.com',
+    'password' => 'Eron@123',
     'port' => '587'
-  );*/
+  );
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-header("Location: index.html");
 
 ?>
